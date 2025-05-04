df3d1fcb0130eedce567ff54fbb2f01dc15a78a6
ZeWeshman <163387467+ZeWeshman@users.noreply.github.com>
2025-05-04 17:41:07 +0000

    made grey theme light grey and fixed the button's color to the BG color for the next theme

--- color_mode_toggler.js
+++ color_mode_toggler.js
@@ -1,4 +1,4 @@
-const themes = ["light", "dark", "grey"];
+const themes = ["grey", "light", "dark"];
 let currentThemeIndex = 0;
 
 function setTheme(theme) {
@@ -16,12 +16,15 @@ function updateButtonColor() {
     const nextThemeIndex = (currentThemeIndex + 1) % themes.length;
     const nextTheme = themes[nextThemeIndex];
     const button = document.querySelector(".mode-button");
+    const tempTheme = document.documentElement.getAttribute("data-theme");
+    document.documentElement.setAttribute("data-theme", nextTheme);
     button.style.backgroundColor = getComputedStyle(document.documentElement)
-        .getPropertyValue(`--button-bg`)
+        .getPropertyValue("--button-bg")
         .trim();
+    document.documentElement.setAttribute("data-theme", tempTheme);
 }
 
 // Load saved theme on page load
-const savedTheme = localStorage.getItem("theme") || "light";
+const savedTheme = localStorage.getItem("theme") || "grey";
 currentThemeIndex = themes.indexOf(savedTheme);
 setTheme(savedTheme);
